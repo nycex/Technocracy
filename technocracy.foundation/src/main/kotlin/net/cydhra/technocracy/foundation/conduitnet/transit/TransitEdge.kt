3b435265d1b15371cf2739bc6b88ca5850d08da6
Cydhra <ubezl@student.kit.edu>
2019-05-29 00:27:13 +0200

    transit edges are of only one type

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduitnet/transit/TransitEdge.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduitnet/transit/TransitEdge.kt
@@ -6,16 +6,12 @@ import net.cydhra.technocracy.foundation.conduitnet.PipeType
 /**
  * @param a first node of the transit edge
  * @param b second node of the transit edge
- * @param types all pipe types on the path between a and b
- * @param capacities the minimum capacity of the path between a and b
+ * @param type pipe type on the path between a and b
+ * @param capacity the minimum capacity of the path between a and b
  * @param cost the amount of nodes between a and b; a inclusive, b exclusive
  */
-class TransitEdge(val a: TransitNode, val b: TransitNode, val types: Array<PipeType>,
-                  val capacities: Array<Int> = Array(types.size) { Integer.MAX_VALUE }, val cost: Int) {
-
-    init {
-        check(types.size == capacities.size) { "less capacity information than types available" }
-    }
+class TransitEdge(val a: TransitNode, val b: TransitNode, val type: PipeType,
+                  val capacity: Int = Integer.MAX_VALUE, val cost: Int) {
 
     /**
      * Offer the first edge for destructuring
