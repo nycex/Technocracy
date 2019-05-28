c89e77d013f735e4d45a0bd7432b98b27c444e24
Cydhra <ubezl@student.kit.edu>
2019-05-29 00:27:13 +0200

    transit edges are of only one type

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduitnet/conduit/ConduitNetworkGatewayNode.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduitnet/conduit/ConduitNetworkGatewayNode.kt
@@ -1,5 +1,6 @@
 package net.cydhra.technocracy.foundation.conduitnet.conduit
 
+import net.cydhra.technocracy.foundation.conduitnet.transit.TransitEdge
 import net.minecraft.util.math.BlockPos
 import java.util.*
 
@@ -7,4 +8,11 @@ import java.util.*
  * @param networkId a unique id of the network this node belongs to
  * @param pos a block position
  */
-open class ConduitNetworkGatewayNode(networkId: UUID, pos: BlockPos) : ConduitNetworkNode(networkId, pos)
+open class ConduitNetworkGatewayNode(networkId: UUID, pos: BlockPos) : ConduitNetworkNode(networkId, pos) {
+
+    /**
+     * A list of transit edges (path-compressed routes) to other transit nodes (like machines and chunk exit
+     * nodes) with their travel cost and flow capacity
+     */
+    val transitEdges = mutableListOf<TransitEdge>()
+}
