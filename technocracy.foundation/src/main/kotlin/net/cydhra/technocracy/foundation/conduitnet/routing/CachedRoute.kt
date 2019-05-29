1f9dbb9aca6eec6fd1f84b91308de9b824619e3c
Cydhra <ubezl@student.kit.edu>
2019-05-29 23:36:27 +0200

    cached routes no longer hold references to chunks

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduitnet/routing/CachedRoute.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduitnet/routing/CachedRoute.kt
@@ -1,7 +1,8 @@
 package net.cydhra.technocracy.foundation.conduitnet.routing
 
-import net.cydhra.technocracy.foundation.conduitnet.NetworkChunk
 import net.cydhra.technocracy.foundation.conduitnet.conduit.ConduitNetworkNode
+import net.cydhra.technocracy.foundation.conduitnet.transit.DimensionNetwork
+import net.minecraft.util.math.ChunkPos
 
 /**
  * A route or a flow (combined from routes) abstracted to its input and output and a list of traversed chunks and
@@ -9,20 +10,24 @@ import net.cydhra.technocracy.foundation.conduitnet.conduit.ConduitNetworkNode
  * content transfer. Clients should call [isStillValid] to check whether the route is still valid or might have
  * changed and should be recalculated.
  *
+ * @param network the global network where this is routed in
  * @param input entry node of the conduit network, supplies something
  * @param output exit node of the conduit network, consumes the supplied good
  * @param capacity how much of the supplied good can be transferred. MAX_INT by default indicating limitless transfer.
  * @param cost path cost for the route. Undefined for flows, as there cannot be two flows between the same in/out nodes
  * @param cacheValidityCounts an array of pairs of saved validity counts and the respective chunk this route traverses
  */
-class CachedRoute(val input: ConduitNetworkNode, val output: ConduitNetworkNode, val capacity: Int = Integer.MAX_VALUE,
-                  val cost: Int, private val cacheValidityCounts: Array<Pair<Int, NetworkChunk>>) {
+class CachedRoute(private val network: DimensionNetwork, val input: ConduitNetworkNode, val output: ConduitNetworkNode,
+                  val capacity: Int = Integer.MAX_VALUE, val cost: Int,
+                  private val cacheValidityCounts: Array<Pair<Int, ChunkPos>>) {
 
     /**
      * @return true, if the route is still unchanged and can be used safely, false if any of the traversed chunks
      * changed thus requiring an update of the route.
      */
     fun isStillValid(): Boolean {
-        return cacheValidityCounts.all { (count, chunk) -> count == chunk.cacheValidationCounter }
+        return cacheValidityCounts.all { (count, chunkPos) ->
+            network.isChunkLoaded(chunkPos) && count == network.getChunk(chunkPos).cacheValidationCounter
+        }
     }
 }
