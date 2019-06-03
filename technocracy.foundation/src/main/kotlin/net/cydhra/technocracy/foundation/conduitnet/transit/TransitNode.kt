334042fbf5779057b840c24b761a7f242f957966
Cydhra <ubezl@student.kit.edu>
2019-06-03 20:47:45 +0200

    added path cost map in transit node

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduitnet/transit/TransitNode.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduitnet/transit/TransitNode.kt
@@ -2,4 +2,10 @@ package net.cydhra.technocracy.foundation.conduitnet.transit
 
 import net.cydhra.technocracy.foundation.conduitnet.conduit.ConduitNetworkGatewayNode
 
-class TransitNode(val conduitNode: ConduitNetworkGatewayNode)
+class TransitNode(val conduitNode: ConduitNetworkGatewayNode) {
+
+    /**
+     * A map where all reachable transit nodes map to the respective path cost to reach them.
+     */
+    val pathCosts = mutableMapOf<TransitNode, Int>()
+}
