daaf7b731f5fda2e9443cd4a542b24a1ceb1a397
Cydhra <ubezl@student.kit.edu>
2019-06-03 18:43:12 +0200

    some doc

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduitnet/conduit/ConduitNetworkNode.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduitnet/conduit/ConduitNetworkNode.kt
@@ -3,6 +3,8 @@ package net.cydhra.technocracy.foundation.conduitnet.conduit
 import net.minecraft.util.math.BlockPos
 
 /**
- * @param pos a block position
+ * The conduit network is a one-to-one representation of a pipe network structure as given by the placement of pipes.
+ *
+ * @param pos the block position of the pipe tile entity that this node is a model of
  */
 abstract class ConduitNetworkNode(private val pos: BlockPos)
