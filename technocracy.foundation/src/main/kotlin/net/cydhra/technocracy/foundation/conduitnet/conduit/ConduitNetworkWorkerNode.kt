c7f287d12d3d7f23c847b33186db521db17eaf48
Cydhra <ubezl@student.kit.edu>
2019-06-03 19:26:51 +0200

    removed Worker node from conduit model, as gateway nodes are sufficient

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduitnet/conduit/ConduitNetworkWorkerNode.kt
+++
@@ -1,8 +0,0 @@
-package net.cydhra.technocracy.foundation.conduitnet.conduit
-
-import net.minecraft.util.math.BlockPos
-
-/**
- * @param pos a block position
- */
-open class ConduitNetworkWorkerNode(pos: BlockPos) : ConduitNetworkNode(pos)
