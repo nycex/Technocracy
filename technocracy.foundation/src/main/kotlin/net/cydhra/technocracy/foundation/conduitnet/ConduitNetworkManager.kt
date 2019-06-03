31c22d61444f1841b918e2b5b4b1ed551f3020e8
Cydhra <ubezl@student.kit.edu>
2019-06-03 18:36:45 +0200

    reimplemented network chunk loading/unloading

--- technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduitnet/ConduitNetworkManager.kt
+++ technocracy.foundation/src/main/kotlin/net/cydhra/technocracy/foundation/conduitnet/ConduitNetworkManager.kt
@@ -1,7 +1,6 @@
 package net.cydhra.technocracy.foundation.conduitnet
 
-import net.cydhra.technocracy.foundation.TCFoundation
-import net.minecraft.util.math.ChunkPos
+import net.cydhra.technocracy.foundation.conduitnet.transit.GlobalTransitNetworks
 import net.minecraftforge.event.world.ChunkDataEvent
 import net.minecraftforge.event.world.ChunkEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
@@ -10,19 +9,18 @@ object ConduitNetworkManager {
 
     private const val NBT_KEY_NETWORK = "network"
 
-    private val networkChunks = HashMap<ChunkPos, NetworkChunk>()
-
     @SubscribeEvent
     fun chunkDataLoad(event: ChunkDataEvent.Load) {
         if (event.world.isRemote)
             return
 
         val networkChunk = NetworkChunk(event.chunk)
-        networkChunks[event.chunk.pos] = networkChunk
 
         if (event.data.hasKey(NBT_KEY_NETWORK)) {
             networkChunk.deserialize(event.data.getCompoundTag(NBT_KEY_NETWORK))
         }
+
+        GlobalTransitNetworks.getNetwork(event.world.provider.dimension).loadNetworkChunk(networkChunk)
     }
 
     @SubscribeEvent
@@ -30,9 +28,8 @@ object ConduitNetworkManager {
         if (event.world.isRemote)
             return
 
-        // save networks
-        event.data.setTag(NBT_KEY_NETWORK, (networkChunks[event.chunk.pos]
-                ?: error("chunk had no network assigned")).serialize())
+        event.data.setTag(NBT_KEY_NETWORK,
+                GlobalTransitNetworks.getNetwork(event.world.provider.dimension).getChunk(event.chunk.pos).serialize())
     }
 
     @SubscribeEvent
@@ -40,7 +37,6 @@ object ConduitNetworkManager {
         if (event.world.isRemote)
             return
 
-        networkChunks.remove(event.chunk.pos)?.unload() ?: TCFoundation.logger
-                .warn("unloaded chunk (${event.chunk.pos}) that had no network assigned")
+        GlobalTransitNetworks.getNetwork(event.world.provider.dimension).unloadNetworkChunk(event.chunk.pos)
     }
 }
